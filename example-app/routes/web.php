aa91dfed91f6fc2c92d24adf6fdc98c02a6166f9
kouta718 <qooron1927@gmail.com>
2025-05-02 23:38:54 +0900

    5/2/2025-pm11:38

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Htpp\Controllers\PostContoroller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('post/create', [PostController::class, 'create']);
