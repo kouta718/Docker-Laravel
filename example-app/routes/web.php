fc0f5f93c2f2ed2e0525d626af40459fa7b70887
kouta718 <qooron1927@gmail.com>
2025-05-09 00:12:22 +0900

    5/9/2025-pm12:10 http://localhost/post/create表示できました

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,10 +1,22 @@
 <?php
 
-use App\Htpp\Controllers\PostContoroller;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('post/create', [PostController::class, 'create']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
+    Route::post('/post', [PostController::class, 'store'])->name('post.store');
+});
+
+require __DIR__.'/auth.php';
