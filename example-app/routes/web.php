f2ea3d7f2abfe0dc219365622b5a50925f21e038
kouta718 <qooron1927@gmail.com>
2025-05-17 14:37:11 +0900

    5/17/2025-pm2:36 ペジネーションの色変更を後回しにして次へ

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -5,6 +5,13 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('post', PostController::class);
+// Route::get('/post', [PostController::class, 'index'])->name('post.index');
+// Route::get('/post/create', [PostController::class, 'create']);
+// Route::post('/post', [PostController::class, 'store'])->name('post.store');
+// Route::get('/post/show/{post}', [PostController::class, 'show'])->name('post.show');
+// Route::get('/post/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
+// Route::patch('/post/{post}', [PostController::class, 'update'])->name('post.update');
+// Route::delete('/post/{post}', [PostController::class, 'destroy'])->name('post.destroy');
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,19 +25,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    // Route::post('/post', [PostController::class, 'store'])->name('post.store');
 });
-
-// // Route::middleware('auth','admin')->group(function () {
-//     Route::get('/post', [PostController::class, 'index'])->name('post.index');
-//     Route::get('/post/create', [PostController::class, 'create']);
-// // });
-
-// Route::get('/post/show/{post}', [PostController::class, 'show'])->name('post.show');
-
-// Route::get('/post/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
-// Route::patch('/post/{post}', [PostController::class, 'update'])->name('post.update');
-
-// Route::delete('/post/{post}', [PostController::class, 'destroy'])->name('post.destroy');
 
 require __DIR__.'/auth.php';
